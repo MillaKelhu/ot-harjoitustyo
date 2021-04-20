4b2efd04769275e4e7f0f3fed958bc51cfe06ac7
Milla Kelhu <milla.kelhu@gmail.com>
2021-04-20 11:21:58 +0300

    Modify database connection, add database solely for testing

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,10 +1,15 @@
 import os
 import sqlite3
+import dotenv
 
 dirname = os.path.dirname(__file__)
 
+dotenv.load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
+
+database_name = os.getenv('database_name')
+
 connection = sqlite3.connect(os.path.join(
-    dirname, "..", "data", "database.sqlite"))
+    dirname, "..", "data", database_name))
 
 
 def get_database_connection():
